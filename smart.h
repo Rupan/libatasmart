ffee9d06d5f95f799621deeffdfce2012275b211
Lennart Poettering <lennart@poettering.net>
2008-06-30 22:13:34 +0200

    fix stupid copynpaste idiocy

--- smart.h
+++ smart.h
@@ -8,18 +8,18 @@
 
     Copyright 2008 Lennart Poettering
 
-    libcanberra is free software; you can redistribute it and/or modify
+    SmartKit is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as
     published by the Free Software Foundation, either version 2.1 of the
     License, or (at your option) any later version.
 
-    libcanberra is distributed in the hope that it will be useful, but
+    SmartKit is distributed in the hope that it will be useful, but
     WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
     Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public
-    License along with libcanberra. If not, If not, see
+    License along with SmartKit. If not, If not, see
     <http://www.gnu.org/licenses/>.
 ***/
 
