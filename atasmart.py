bde6dea4bc32e8a55fee0ea9037f7c073f86f902
Michael Mohr <akihana@gmail.com>
2014-02-22 23:50:44 -0800

    Add more tests

--- atasmart.py
+++ atasmart.py
@@ -11,8 +11,13 @@ try:
     id = sk.identify()
     for key in id.keys():
         print key + ' -> ' + id[key]
-    print "Device temperature: " + str(sk.temp())
-    print 'Checking S.M.A.R.T. status: ' + str(sk.smart_status())
+    sk.refresh()
+    #print "Device temperature: " + str(sk.temp())
+    print 'SMART status is good: ' + str(sk.status())
+    print 'Power cycle count: ' + str(sk.power_cycles())
+    print 'Power on time: ' + str(sk.power_on())
+    print 'Bad sectors: ' + str(sk.bad_sectors())
+    print 'Overall: ' + str(sk.overall())
     sk.close()
 except SmartError, smart_re:
     print '%s: ' % diskdev + str(smart_re)
